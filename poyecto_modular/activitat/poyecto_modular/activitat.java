cba67402fbcf250a337bc138979b9d39c0513e33
Aarongp13 <aarongarcia132005@gmail.com>
2025-02-20 22:29:53 +0100

    Fer la primera part del exercici
    En el meu cas e demanat al usuari que digues el nom de la herba i un cop aixo busque la herba per el nom i te acabe dien el oigen, la herba i el preu i si no se trobe pues un misatge de no sa trobat

--- poyecto_modular/activitat/poyecto_modular/activitat.java
+++ poyecto_modular/activitat/poyecto_modular/activitat.java
@@ -1,11 +1,15 @@
 package poyecto_modular;
 
+import java.util.Scanner;
+
 public class activitat {
 
 	public class main {
 		public static String[] hierba = { "Maria Juana", "Hierba de los bosques", "La seta feliz", "El oro verde" };
 		public static String[] origen = { "Mexico", "Andorra", "Marrueco", "Palestina" };
 		public static double[] precio = { 1.70, 4.20, 2.47, 1.33 };
+		
+		static Scanner scanner = new Scanner(System.in);
 
 		// Variables publicas extra que considereis
 
@@ -18,7 +22,28 @@ public class activitat {
 			System.out.println("Usuario selecciona buscar por nombre");
 			nombre = "Juana";
 			// Codigo para buscar por nombre
-
+			
+			System.out.println("Dime el nombre de la hierba quequieres buscar: ");
+			String nombre1 = scanner.nextLine();
+			
+			for (int i = 0; i < hierba.length; i++) {
+				if (hierba[i].equals(nombre1)) {
+					System.out.println("Encontrado " + hierba[i]);
+					System.out.println("Origen: " + origen[i]);
+					System.out.println("Precio" + precio[i]);
+					return;
+				} 
+				
+				{
+					
+				}
+				
+				System.out.println("No se encontro ninguna hierba con este nombre ");
+				
+			
+			}
+						
+			
 			System.out.println("Usuario selecciona buscar por origen");
 			lugar = "Andorra";
 			// Codigo para buscar por origen
@@ -31,15 +56,15 @@ public class activitat {
 
 			System.out.println("Usuario selecciona varios productos para comprar");
 			// Codigo para seleccionar primer producto
-			nombre = "Hierba de los bosques";
+			nombre1 = "Hierba de los bosques";
 			cantidad = 3;
 
 			// Codigo para seleccionar segundo producto
-			nombre = "El oro verde";
+			nombre1 = "El oro verde";
 			cantidad = 1;
 
 			// Codigo para seleccionar tercer producto
-			nombre = "La seta feliz";
+			nombre1 = "La seta feliz";
 			cantidad = 2.5;
 			
 			System.out.println("Usuario elimina el segundo elemento de su carrito");
